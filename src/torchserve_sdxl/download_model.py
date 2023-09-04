d5d1a9100284ce5bc4a4a19585ee08bbf8398940
RSWAIN1486 <rswain1486@gmail.com>
2023-09-05 02:29:43 +0530

    Download SDXL model

---
+++ src/torchserve_sdxl/download_model.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from diffusers import StableDiffusionXLPipeline
+import torch
+
+pipe = StableDiffusionXLPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, variant="fp16", use_safetensors=True
+)
+
+pipe.save_pretrained("./sdxl-1.0-model")
